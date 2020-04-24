5ddb1af9e29068af7f81bd084f129597e16d0493
David Thole <david@thedarktrumpet.com>
2020-04-24 15:46:00 -0500

    Fleshing out the person model, for what I'm going to care about, from the request

--- libTeamwork/api/Tasks.cs
+++ libTeamwork/api/Tasks.cs
@@ -2,6 +2,7 @@ namespace libTeamwork.api
 {
     public class Tasks : Base
     {
+        public long Id { get; set; }
         
     }
 }
--- libTeamwork/models/Person.cs
+++ libTeamwork/models/Person.cs
@@ -1,7 +1,15 @@
+using System;
+
 namespace libTeamwork.models
 {
     public class Person
     {
-        
+        public long Id { get; set; }
+        public string UserName { get; set; }
+        public DateTime LastActive { get; set; }
+        public string FullName { get; set; }
+        public string EmailAddress { get; set; }
+        public string CompanyName { get; set; }
+        public bool Administrator { get; set; }
     }
 }
