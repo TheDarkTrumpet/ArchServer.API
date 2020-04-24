8b365c9671618fcaa7adb98bd089bce775759809
David Thole <david@thedarktrumpet.com>
2020-04-24 15:47:20 -0500

    Bulk copying base from toggl, adding json and restsharp

--- libTeamwork/api/Base.cs
+++ libTeamwork/api/Base.cs
@@ -2,6 +2,24 @@ namespace libTeamwork.api
 {
     public class Base
     {
+        protected string ApiKey { get; set; }
+        protected string BaseURL { get; set; }
         
+        protected CookieContainer CookieContainer { get; set; }
+        protected IRestClient RestClient { get; set; }
+        
+        protected Base(string apiKey)
+        {
+            ApiKey = apiKey;
+            BaseURL = "https://www.toggl.com/api/v8";
+            CookieContainer = new CookieContainer();
+        }
+
+        protected void CreateClient()
+        {
+            RestClient = new RestClient(BaseURL);
+            RestClient.Authenticator = new HttpBasicAuthenticator(ApiKey, "api_token");
+            RestClient.CookieContainer = CookieContainer;
+        }
     }
 }
