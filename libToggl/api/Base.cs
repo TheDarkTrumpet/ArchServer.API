80486a8c206014365d5cf7eb691515ff70471327
David Thole <david@thedarktrumpet.com>
2020-04-24 09:44:59 -0500

    Adding in much of the base on what it should contain

--- libToggl/api/Base.cs
+++ libToggl/api/Base.cs
@@ -4,5 +4,13 @@ namespace libToggl.api
 {
     public class Base
     {
+        protected string ApiKey { get; set; }
+        protected string BaseURL { get; set; }
+
+        public Base(string apiKey)
+        {
+            ApiKey = apiKey;
+            BaseURL = "https://www.toggl.com/api/v8";
+        }
     }
 }
