542bd1086ec729eaec51b046e846ab2aca4270c6
David Thole <david@thedarktrumpet.com>
2020-04-25 23:11:18 -0500

    Adding elements to the teamwork task model.

--- libAPICache/Models/Teamwork/Task.cs
+++ libAPICache/Models/Teamwork/Task.cs
@@ -1,7 +1,27 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace libAPICache.Models.Teamwork
 {
+    [Table(name: "Task", Schema = "Teamwork")]
     public class Task
     {
-        
+        [Key]
+        public long Id { get; set; }
+        [MaxLength(255)]
+        public string ProjectName { get; set; }
+        [MaxLength(255)]
+        public string CompanyName { get; set; }
+        [MaxLength(255)]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        [MaxLength(20)]
+        public string Priority { get; set; }
+        [MaxLength(50)]
+        public string AssignedTo { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public DateTime? DueDate { get; set; }
+        public Boolean Completed { get; set; }
     }
 }
