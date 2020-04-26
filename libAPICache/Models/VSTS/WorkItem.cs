c9ddc18060bac604fda91650d7229282678d0e24
David Thole <david@thedarktrumpet.com>
2020-04-26 09:26:55 -0500

    Adding models for the VSTS part of EF

--- libAPICache/Models/VSTS/WorkItem.cs
+++ libAPICache/Models/VSTS/WorkItem.cs
@@ -1,7 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace libAPICache.Models.VSTS
 {
     public class WorkItem
     {
-        
+        public WorkItem()
+        {
+            Comments = new List<WorkItemComment>();
+        }
+        [Key]
+        public int id { get; set; }
+        [MaxLength(255)]
+        public string url { get; set; }
+        [MaxLength(50)]
+        public string Type { get; set; }
+        [MaxLength(50)]
+        public string State { get; set; }
+        public string Description { get; set; }
+        [MaxLength(100)]
+        public string AssignedTo { get; set; }
+        [MaxLength(100)]
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ChangedDate { get; set; }
+        public List<WorkItemComment> Comments { get; set; }
     }
 }
--- libAPICache/Models/VSTS/WorkItemComment.cs
+++ libAPICache/Models/VSTS/WorkItemComment.cs
@@ -1,7 +1,17 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace libAPICache.Models.VSTS
 {
+    [Table(name: "WorkItemComment", Schema = "VSTS")]
     public class WorkItemComment
     {
-        
+        [Key]
+        public int id { get; set; }
+        [MaxLength(200)]
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string Comment { get; set; }
     }
 }
