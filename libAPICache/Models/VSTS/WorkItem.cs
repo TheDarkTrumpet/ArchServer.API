0b33a3085ebc389c82366d54fdd2c490bcc69d94
David Thole <david@thedarktrumpet.com>
2020-04-26 13:36:56 -0500

    Fixing the table/schema for the work item table

--- libAPICache/Models/VSTS/WorkItem.cs
+++ libAPICache/Models/VSTS/WorkItem.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace libAPICache.Models.VSTS
 {
+    [Table(name: "WorkItems", Schema = "VSTS")]
     public class WorkItem
     {
         public WorkItem()
