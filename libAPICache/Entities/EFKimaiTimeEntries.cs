970873195fe7b8af5a54231b8d8cfed43205f625
David Thole <david@thedarktrumpet.com>
2020-04-26 16:03:47 -0500

    Adding some methods for the EFKimaiTimeEntries, some stubs.

--- libAPICache/Entities/EFKimaiTimeEntries.cs
+++ libAPICache/Entities/EFKimaiTimeEntries.cs
@@ -1,4 +1,9 @@
+using System.Collections.Generic;
+using System.Linq;
 using libAPICache.Abstract;
+using libAPICache.Models.Kimai;
+using libAPICache.util;
+using libKimai.models;
 
 namespace libAPICache.Entities
 {
@@ -10,6 +15,33 @@ namespace libAPICache.Entities
         public EFKimaiTimeEntries(EFDbContext context)
         {
             _context = context;
+        }
+
+        public IEnumerable<TimeEntry> TimeEntries => _context.KimaiTimeEntries;
+
+        public bool SaveEntry(Activity timeEntry)
+        {
+            TimeEntry saveTimeEntry = new TimeEntry();
+
+            saveTimeEntry.Copy(timeEntry);
+            SaveEntry(saveTimeEntry);
+
+            return true;
+        }
+
+        public bool SaveEntry(TimeEntry timeEntry)
+        {
+            TimeEntry srcEntry = GetOrReturnNull(timeEntry.Id);
+
+            if (srcEntry == null)
+            {
+                
+            }
+        }
+
+        public TimeEntry GetOrReturnNull(int id)
+        {
+            return TimeEntries.FirstOrDefault(x => x.Id == id);
         }
     }
 }
