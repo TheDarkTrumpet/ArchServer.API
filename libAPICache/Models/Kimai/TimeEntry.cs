19ee08b083f1dbf43795cc4d1ec8247ec76e4412
David Thole <david@thedarktrumpet.com>
2020-04-25 23:07:43 -0500

    Starting to add the models and content that represents them

--- libAPICache/Models/Kimai/TimeEntry.cs
+++ libAPICache/Models/Kimai/TimeEntry.cs
@@ -1,7 +1,25 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace libAPICache.Models.Kimai
 {
+    [Table(name: "TimeEntries", Schema = "Kimai")]
     public class TimeEntry
     {
-        
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(255)]
+        public string ActivityName { get; set; }
+        public string ActivityComment { get; set; }
+        [MaxLength(255)]
+        public string ProjectName { get; set; }
+        public string ProjectComment { get; set; }
+        [MaxLength(255)]
+        public string Customer { get; set; }
+        public double? HourlyRate { get; set; }
+        public DateTime? StartTime { get; set; }
+        public DateTime? EndTime { get; set; }
+        public string TimeNotes { get; set; }
     }
 }
--- libAPICache/Models/Teamwork/Person.cs
+++ libAPICache/Models/Teamwork/Person.cs
@@ -1,7 +1,23 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace libAPICache.Models.Teamwork
 {
+    [Table(name: "Person", Schema = "TeamWork")]
     public class Person
     {
-        
+        [Key]
+        public long Id { get; set; }
+        [MaxLength(100)]
+        public string UserName { get; set; }
+        public DateTime? LastActive { get; set; }
+        [MaxLength(100)]
+        public string FullName { get; set; }
+        [MaxLength(100)]
+        public string EmailAddress { get; set; }
+        [MaxLength(255)]
+        public string CompanyName { get; set; }
+        public bool Administrator { get; set; }
     }
 }
