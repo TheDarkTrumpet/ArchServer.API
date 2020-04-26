f93e31a03c58ac64bb87cd0c598b13f001a562bb
David Thole <david@thedarktrumpet.com>
2020-04-25 23:07:55 -0500

    Changing the last active to nullable

--- libTeamwork/models/Person.cs
+++ libTeamwork/models/Person.cs
@@ -6,7 +6,7 @@ namespace libTeamwork.models
     {
         public long Id { get; set; }
         public string UserName { get; set; }
-        public DateTime LastActive { get; set; }
+        public DateTime? LastActive { get; set; }
         public string FullName { get; set; }
         public string EmailAddress { get; set; }
         public string CompanyName { get; set; }
