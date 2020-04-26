f4cd132bde05966872f55325fd5ca48425044b8b
David Thole <david@thedarktrumpet.com>
2020-04-25 22:58:05 -0500

    Adding model bindings to the EFDbContext

--- libAPICache/Entities/EFDbContext.cs
+++ libAPICache/Entities/EFDbContext.cs
@@ -1,7 +1,15 @@
+using System.Data.Entity;
+using libAPICache.Models;
+
 namespace libAPICache.Entities
 {
-    public class EFDbContext
+    public class EFDbContext : DbContext
     {
-        
+        public DbSet<Models.Kimai.TimeEntry> KimaiTimeEntries {get;set;}
+        public DbSet<Models.Teamwork.Person> TeamworkPeople { get; set; }
+        public DbSet<Models.Teamwork.Task> TeamworkTasks { get; set; }
+        public DbSet<Models.Toggl.TimeEntry> TogglTimeEntries { get; set; }
+        public DbSet<Models.VSTS.WorkItem> VSTSWorkItems { get; set; }
+        public DbSet<Models.VSTS.WorkItemComment> VSTSWorkItemComments { get; set; }
     }
 }
