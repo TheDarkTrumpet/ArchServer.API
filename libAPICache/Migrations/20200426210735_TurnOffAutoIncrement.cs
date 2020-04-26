cba72d43f20b4b148355c3f40337e133c4305f3b
David Thole <david@thedarktrumpet.com>
2020-04-26 16:10:24 -0500

    Adding migration, despite it not working

---
+++ libAPICache/Migrations/20200426210735_TurnOffAutoIncrement.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace libAPICache.Migrations
+{
+    public partial class TurnOffAutoIncrement : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "id",
+                schema: "VSTS",
+                table: "WorkItems",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "id",
+                schema: "VSTS",
+                table: "WorkItemComment",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                schema: "Toggl",
+                table: "TimeEntries",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                schema: "TeamWork",
+                table: "People",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                schema: "Teamwork",
+                table: "Tasks",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                schema: "Kimai",
+                table: "TimeEntries",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "id",
+                schema: "VSTS",
+                table: "WorkItems",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int))
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "id",
+                schema: "VSTS",
+                table: "WorkItemComment",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int))
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                schema: "Toggl",
+                table: "TimeEntries",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(long))
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                schema: "TeamWork",
+                table: "People",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(long))
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                schema: "Teamwork",
+                table: "Tasks",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(long))
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                schema: "Kimai",
+                table: "TimeEntries",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int))
+                .Annotation("SqlServer:Identity", "1, 1");
+        }
+    }
+}
