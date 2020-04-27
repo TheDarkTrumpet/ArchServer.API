0b7a573d18921aa3d282d7ac5f9f7d92c2610aa4
David Thole <david@thedarktrumpet.com>
2020-04-27 10:09:24 -0500

    Adding the body to the cache method for workspaces, adding interface, and associated linkages

--- libAPICache/Abstract/ITogglWorkspace.cs
+++ libAPICache/Abstract/ITogglWorkspace.cs
@@ -1,7 +1,7 @@
 namespace libAPICache.Abstract
 {
-    public interface ITogglWorkspace
+    public interface ITogglWorkspace : IBase<Models.Toggl.Workspace, libToggl.models.Workspace>
     {
-        
+        void CacheEntries();
     }
 }
--- libAPICache/Entities/EFTogglWorkspace.cs
+++ libAPICache/Entities/EFTogglWorkspace.cs
@@ -1,10 +1,22 @@
+using System.Collections.Generic;
 using libAPICache.Abstract;
 using libAPICache.Models.Toggl;
+using libToggl.api;
 
 namespace libAPICache.Entities
 {
     public class EFTogglWorkspace : EFBase<Workspace, libToggl.models.Workspace>, ITogglWorkspace
     {
+        public EFTogglWorkspace() : this(new EFDbContext()) { }
+        public EFTogglWorkspace(EFDbContext context) : base(context) { }
         
+        public void CacheEntries()
+        {
+            string apiKey = GetAPIKey("APISources:Toggl");
+            Workspaces workspaces = new Workspaces(apiKey);
+
+            List<libToggl.models.Workspace> workspaceList = workspaces.GetWorkspaces();
+            SaveEntries(workspaceList);
+        }
     }
 }
