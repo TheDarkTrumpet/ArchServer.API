34e9bf0d050db6975700341649d47c55ca479674
David Thole <david@thedarktrumpet.com>
2020-04-27 10:12:41 -0500

    Adding migration for toggl workspaces

---
+++ libAPICache/Migrations/20200427151211_AddTogglWorkspaces.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace libAPICache.Migrations
+{
+    public partial class AddTogglWorkspaces : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Workspaces",
+                schema: "Toggl",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false),
+                    Name = table.Column<string>(maxLength: 100, nullable: true),
+                    Premium = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Workspaces", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Workspaces",
+                schema: "Toggl");
+        }
+    }
+}
