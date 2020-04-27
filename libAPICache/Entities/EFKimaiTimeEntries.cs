967d5c08ec6731f79d023cd4feaf2fc669725c6f
David Thole <david@thedarktrumpet.com>
2020-04-26 19:53:45 -0500

    Adding the remainder of the code that should surround what I need here

--- libAPICache/Entities/EFKimaiTimeEntries.cs
+++ libAPICache/Entities/EFKimaiTimeEntries.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using libAPICache.Abstract;
 using libAPICache.Models.Kimai;
 using libAPICache.util;
 using libKimai.models;
+using libKimai.query;
+using Microsoft.Extensions.Configuration;
 
 namespace libAPICache.Entities
 {
@@ -33,17 +36,50 @@ namespace libAPICache.Entities
         {
             TimeEntry srcEntry = GetOrReturnNull(timeEntry.Id);
 
-            if (srcEntry == null)
+            if (srcEntry != null)
             {
-                
+                _context.Entry(srcEntry).CurrentValues.SetValues(timeEntry);
+            }
+            else
+            {
+                _context.KimaiTimeEntries.Add(timeEntry);
             }
 
+            _context.SaveChanges();
             return true;
         }
 
+        public bool SaveEntries(List<Activity> timeEntries)
+        {
+            bool result = false;
+            foreach (var te in timeEntries)
+            {
+                result = SaveEntry(te);
+            }
+
+            return result;
+        }
+        
         public TimeEntry GetOrReturnNull(int id)
         {
             return TimeEntries.FirstOrDefault(x => x.Id == id);
+        }
+
+        public void CacheEntries(DateTime? fromDate = null)
+        {
+            IConfiguration config = util.Configuration.GetConfiguration();
+            string connectionString = (string) config["APISources:Kimai"];
+
+            if (String.IsNullOrEmpty(connectionString))
+            {
+                throw new Exception(
+                    "MySQL Connection string was not found in the appsettings, please ensure it's there before running this");
+            }
+            
+            Activities activities = new Activities(connectionString);
+
+            activities.FromDate = fromDate;
+            List<Activity> results = activities.GetActivities();
         }
     }
 }
