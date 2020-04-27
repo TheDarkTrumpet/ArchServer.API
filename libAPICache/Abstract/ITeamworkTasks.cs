f2773248eb9504e89544d100581656bbc892827e
David Thole <david@thedarktrumpet.com>
2020-04-27 09:15:29 -0500

    Added the cache call to teamwork tasks

--- libAPICache/Abstract/ITeamworkTasks.cs
+++ libAPICache/Abstract/ITeamworkTasks.cs
@@ -1,7 +1,9 @@
+using System;
+
 namespace libAPICache.Abstract
 {
     public interface ITeamworkTasks : IBase<Models.Teamwork.Task, libTeamwork.models.Task>
     {
-        
+        void CacheEntries(DateTime? fromDate = null, bool includeCompleted = true);
     }
 }
--- libAPICache/Entities/EFTeamworkTasks.cs
+++ libAPICache/Entities/EFTeamworkTasks.cs
@@ -1,14 +1,31 @@
 using System;
+using System.Collections.Generic;
 using libAPICache.Abstract;
+using libAPICache.Models.Teamwork;
+using libTeamwork.api;
 
 namespace libAPICache.Entities
 {
     public class EFTeamworkTasks : EFBase<Models.Teamwork.Task, libTeamwork.models.Task>, ITeamworkTasks
     {
-        public void CacheEntries(DateTime? fromDate)
+        private readonly string _baseURL;
+        public EFTeamworkTasks(string baseUrl) : this(new EFDbContext(), baseUrl) { }
+
+        public EFTeamworkTasks(EFDbContext context, string baseUrl) : base(context)
+        {
+            _baseURL = baseUrl;
+            Entries = _dbSet = _context.TeamworkTasks;
+        }
+        
+        public void CacheEntries(DateTime? fromDate = null, bool includeCompleted = true)
         {
             string apiKey = GetAPIKey("APISources:Teamwork");
-            
+            Tasks tasks = new Tasks(apiKey, _baseURL);
+            tasks.UpdatedAfterDate = fromDate;
+            tasks.IncludeCompleted = true;
+
+            List<libTeamwork.models.Task> taskList = tasks.GetTasks();
+            SaveEntries(taskList);
         }
     }
 }
