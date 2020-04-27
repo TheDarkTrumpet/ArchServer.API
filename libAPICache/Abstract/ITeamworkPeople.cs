b3c2350119686e89cf8217dfc3d6cdbcbb26c4fc
David Thole <david@thedarktrumpet.com>
2020-04-27 09:39:23 -0500

    Adding an interface and functionality for Teamwork People

---
+++ libAPICache/Abstract/ITeamworkPeople.cs
@@ -0,0 +1,7 @@
+namespace libAPICache.Abstract
+{
+    public interface ITeamworkPeople
+    {
+        void CacheEntries();
+    }
+}
---
+++ libAPICache/Entities/EFTeamworkPeople.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using libAPICache.Abstract;
+using libTeamwork.api;
+
+namespace libAPICache.Entities
+{
+    public class EFTeamworkPeople: EFBase<Models.Teamwork.Person, libTeamwork.models.Person>, ITeamworkPeople
+    {
+        public EFTeamworkPeople() : this(new EFDbContext()) { }
+
+        public EFTeamworkPeople(EFDbContext context) : base(context)
+        {
+            Entries = _dbSet = _context.TeamworkPeople;
+        }
+
+        public void CacheEntries()
+        {
+            string apiKey = GetAPIKey("APISources:Teamwork");
+            string baseURL = GetAPIKey("APISources:TeamworkURL");
+            People people = new People(apiKey, baseURL);
+
+            List<libTeamwork.models.Person> peopleList = people.GetPeople();
+            SaveEntries(peopleList);
+        }
+    }
+}
