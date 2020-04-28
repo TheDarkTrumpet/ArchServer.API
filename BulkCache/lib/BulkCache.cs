66ea96e8ce4edd30101325f170f834daf4cfeefa
David Thole <david@thedarktrumpet.com>
2020-04-28 11:47:40 -0500

    Writing a bunch of helper methods to help get the settings from the configuration file, and starting to add the cache routines

--- BulkCache/lib/BulkCache.cs
+++ BulkCache/lib/BulkCache.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using libAPICache.Abstract;
 using libAPICache.Entities;
 using libAPICache.util;
@@ -25,7 +27,7 @@ namespace BulkCache.lib
         protected void CacheKimai()
         {
             IKimaiTimeEntries efKimai = new EFKimaiTimeEntries();
-            efKimai.CacheEntries();
+            efKimai.CacheEntries(GetFromDay("Kimai:FromDateDays"));
         }
 
         protected void CacheToggl()
@@ -41,6 +43,35 @@ namespace BulkCache.lib
         protected void CacheVSTS()
         {
             
+        }
+
+        private List<string> GetFromCollection(string identifier)
+        {
+            List<string> returnValues = new List<string>();
+
+            return returnValues;
+        }
+        
+        private DateTime GetFromDay(string identifier)
+        {
+            string value = GetFromConfig(identifier);
+            int fromDay = int.Parse(value);
+
+            return DateTime.Now.AddDays(fromDay);
+        }
+        
+        private string GetFromConfig(string identifier)
+        {
+            string fullLookup = $"APISources:identifier";
+            string value = (string) _configuration[fullLookup];
+            
+            if (String.IsNullOrEmpty(value))
+            {
+                throw new Exception(
+                    $"appsettings.json entry does not exist for {identifier}, please make sure it's defined!");
+            }
+
+            return value;
         }
     }
 }
