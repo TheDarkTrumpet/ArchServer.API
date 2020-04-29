04f1911f5dbebe472d9fa4b42eb5d67d1bf92dd3
David Thole <david@thedarktrumpet.com>
2020-04-29 09:18:36 -0500

    Attempt at the first test, removing automoq and adding automoqcore

--- libAPICache.tests/Models/Kimai/TimeEntryTests.cs
+++ libAPICache.tests/Models/Kimai/TimeEntryTests.cs
@@ -1,3 +1,6 @@
+using AutoFixture;
+using libAPICache.Models.Kimai;
+using libAPICache.util;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace libAPICache.tests.Models.Kimai
@@ -5,6 +8,16 @@ namespace libAPICache.tests.Models.Kimai
     [TestClass]
     public class TimeEntryTests
     {
-        
+        [TestMethod]
+        public void TestProperties()
+        {
+            var fixture = new Fixture();
+            var expected = fixture.Build<TimeEntry>()
+                .OmitAutoProperties()
+                .Create();
+            TimeEntry result = new TimeEntry().Copy(expected);
+            
+            Assert.AreSame(expected, result);
+        }
     }
 }
