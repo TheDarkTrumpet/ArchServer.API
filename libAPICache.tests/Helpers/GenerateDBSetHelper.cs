49374031a8666613a0da0c1657483c06c209842b
David Thole <david@thedarktrumpet.com>
2020-04-30 09:36:35 -0500

    Adding a helper class for generating the DBset

---
+++ libAPICache.tests/Helpers/GenerateDBSetHelper.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using System.Linq;
+using libAPICache.Models;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+
+namespace libAPICache.tests.Helpers
+{
+    public static class GenerateDBSetHelper<T> where T: Base, new()
+    {
+        public static Mock<DbSet<T>> GenerateDbSet(IEnumerable<T> elements)
+        {
+            Mock<DbSet<T>> mockSet = new Mock<DbSet<T>>();
+            mockSet.As<IQueryable<T>>().Setup(m => m.Provider).Returns(elements.Provider);
+            mockSet.As<IQueryable<T>>().Setup(m => m.Expression).Returns(elements.Expression);
+            mockSet.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(elements.ElementType);
+            mockSet.As<IQueryable<T>>().Setup(m => m.GetEnumerator())
+                .Returns(elements.GetEnumerator());
+
+            return mockSet;
+        }
+    }
+}
