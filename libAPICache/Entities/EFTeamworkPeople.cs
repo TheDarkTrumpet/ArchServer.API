1382d68f4e31f48f95731d03f2e89c9ab05a9ccb
David Thole <david@thedarktrumpet.com>
2020-05-01 16:20:06 -0500

    Refactoring EFTeamworkPeople to better support the redirection we need for the tests

--- libAPICache/Entities/EFTeamworkPeople.cs
+++ libAPICache/Entities/EFTeamworkPeople.cs
@@ -9,20 +9,24 @@ namespace libAPICache.Entities
 {
     public sealed class EFTeamworkPeople: EFBase<Models.Teamwork.Person, libTeamwork.models.Person>, ITeamworkPeople
     {
+        private IPeople _iPeople;
+        public string ApiKey { get; set; }
+        public string BaseUrl { get; set; }
         public EFTeamworkPeople() : this(new EFDbContext(), new Config()) { }
 
-        public EFTeamworkPeople(EFDbContext context, IConfig configuration) : base(context, configuration)
+        public EFTeamworkPeople(EFDbContext context, IConfig configuration, IPeople people = null) : base(context, configuration)
         {
             Entries = DbSet = Context.TeamworkPeople;
+            
+            ApiKey = Configuration.GetKey("APISources:Teamwork:API_Key");
+            BaseUrl = Configuration.GetKey("APISources:Teamwork:Base_URL");
+            
+            _iPeople = people ?? new People(ApiKey, BaseUrl);
         }
 
         public void CacheEntries()
         {
-            string apiKey = Configuration.GetKey("APISources:Teamwork:API_Key");
-            string baseURL = Configuration.GetKey("APISources:Teamwork:Base_URL");
-            People people = new People(apiKey, baseURL);
-
-            List<libTeamwork.models.Person> peopleList = people.GetPeople();
+            List<libTeamwork.models.Person> peopleList = _iPeople.GetPeople();
             SaveEntries(peopleList);
         }
     }
