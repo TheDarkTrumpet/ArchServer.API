2feb795048e7d3582010915dbe4462adc47ccb87
David Thole <david@thedarktrumpet.com>
2020-05-04 10:05:19 -0500

    Adding duration migration

---
+++ libAPICache/Migrations/20200504150453_KimaiDuration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace libAPICache.Migrations
+{
+    public partial class KimaiDuration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Duration",
+                schema: "Kimai",
+                table: "TimeEntries",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Duration",
+                schema: "Kimai",
+                table: "TimeEntries");
+        }
+    }
+}
