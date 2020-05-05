269414bb3e98adcd4efb545bde4d8591bb17dc40
David Thole <david@thedarktrumpet.com>
2020-05-05 12:14:56 -0500

    Adding setups for the upcoming tests

--- External.tests/libTeamworkTests/People.cs
+++ External.tests/libTeamworkTests/People.cs
@@ -1,3 +1,4 @@
+using libTeamwork.api;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using RestSharp;
@@ -7,15 +8,47 @@ namespace External.tests.libTeamworkTests
     [TestClass]
     public class People
     {
+        private libTeamwork.api.People _people { get; set; }
+        public Mock<RestRequest> MockRestRequest { get; set; }
+        
+        public Mock<RestClient> MockRestClient { get; set; }
+        
+        [TestMethod]
+        public void Constructor_WithAPIAndBase_ShouldSetProperties()
+        {
+            Assert.IsNotNull(_people.ApiKey);
+            Assert.IsNotNull(_people.RestClient);
+            Assert.IsNotNull(_people.CookieContainer);
+            Assert.IsNotNull(_people.RestRequest);
+            Assert.IsNotNull(_people.RestClient);
+            Assert.IsNotNull(_people.BaseURL);
+            Assert.IsNotNull(_people.EndPointURI);
+        }
 
+        [TestMethod]
+        public void GetRawPeople_ShouldReturnJArrayObject()
+        {
+            
+        }
+
+        [TestMethod]
+        public void GetPeople_WithAvailableElements_ShouldReturnListOfPeople()
+        {
+            
+        }
+        
         [TestInitialize]
         public void Initialize()
         {
-            /*
-            Mock<RestRequest> mockRestRequest = new Mock<RestRequest>();
-            Mock<RestClient> mockRestClient = new Mock<RestClient>();
-            mockRestClient.Setup(x => x.Execute(It))
-            */
+            _people = new libTeamwork.api.People("AnAPIKey", "/peoplemock.json");
+            MockRestRequest = new Mock<RestRequest>();
+            MockRestClient = new Mock<RestClient>();
+            MockRestClient.Setup(x => x.Execute(MockRestRequest.Object));
+        }
+
+        private class PeopleMock
+        {
+            
         }
     }
 }
