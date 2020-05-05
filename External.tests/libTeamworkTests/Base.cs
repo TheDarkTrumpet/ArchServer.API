478147f544c7d413d8689ac0a9fdcbc973c6a757
David Thole <david@thedarktrumpet.com>
2020-05-04 21:34:41 -0500

    Adding the 2nd test, to the teamwork api tests, and some refactoring

--- External.tests/libTeamworkTests/Base.cs
+++ External.tests/libTeamworkTests/Base.cs
@@ -8,21 +8,41 @@ namespace External.tests.libTeamworkTests
     [TestClass]
     public class Base
     {
+        private BaseMock _baseMock;
+        
         [TestMethod]
         public void Constructor_WithParameters_ShouldCreateVariables()
         {
-            BaseMock baseMock  = new BaseMock("A Key", "http://google.com");
+            Assert.IsNotNull(_baseMock.ApiKey);
+            Assert.IsNotNull(_baseMock.BaseURL);
+            Assert.IsNotNull(_baseMock.CookieContainer);
+        }
+
+        [TestMethod]
+        public void CreateClient_ShouldCreateClientAndSetProperties()
+        {
+            _baseMock.CallCreateClient();
             
-            Assert.IsNotNull(baseMock.ApiKey);
-            Assert.IsNotNull(baseMock.BaseURL);
-            Assert.IsNotNull(baseMock.CookieContainer);
+            Assert.IsNotNull(_baseMock.RestClient);
+            Assert.IsNotNull(_baseMock.RestClient.Authenticator);
+            Assert.IsNotNull(_baseMock.RestClient.CookieContainer);
+        }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _baseMock  = new BaseMock("A Key", "http://google.com");
         }
 
         private class BaseMock : libTeamwork.api.Base
         {
-
             public BaseMock(string apiKey, string baseUrl) : base(apiKey, baseUrl)
             {
+            }
+
+            public void CallCreateClient()
+            {
+                base.CreateClient();
             }
         }
     }
