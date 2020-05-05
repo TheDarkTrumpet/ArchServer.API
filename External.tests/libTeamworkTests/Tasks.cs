34cd29fcd93ecc0a4f3d836d8f396ed091bdb937
David Thole <david@thedarktrumpet.com>
2020-05-05 14:19:15 -0500

    Adding the second and final test

--- External.tests/libTeamworkTests/Tasks.cs
+++ External.tests/libTeamworkTests/Tasks.cs
@@ -1,7 +1,10 @@
 using System.Collections.Generic;
+using System.Linq;
+using libTeamwork.models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using RestSharp;
 
 namespace External.tests.libTeamworkTests
@@ -12,8 +15,51 @@ namespace External.tests.libTeamworkTests
         private TasksMock _tasksMock;
         private Mock<RestRequest> MockRestRequest { get; set; }
         private Mock<RestClient> MockRestClient { get; set; }
-        private Dictionary<string, List<Dictionary<string, string>>> InputObject;        
+        private Dictionary<string, List<Dictionary<string, string>>> InputObject;
 
+        [TestMethod]
+        public void Constructor_WithAPIAndBase_ShouldSetProperties()
+        {
+            Assert.IsNotNull(_tasksMock.ApiKey);
+            Assert.IsNotNull(_tasksMock.RestClient);
+            Assert.IsNotNull(_tasksMock.CookieContainer);
+            Assert.IsNotNull(_tasksMock.RestRequest);
+            Assert.IsNotNull(_tasksMock.RestClient);
+            Assert.IsNotNull(_tasksMock.BaseURL);
+            Assert.IsNotNull(_tasksMock.EndPointURI);
+        }
+
+        [TestMethod]
+        public void GetRawTasks_ShouldReturnJArrayObject()
+        {
+            JArray response = _tasksMock.GetRawTasks();
+            Assert.IsNotNull(response);
+            Assert.AreEqual(1, response.Count);
+        }
+
+        [TestMethod]
+        public void GetTasks_WithAvailableElements_ShouldReturnListOfTasks()
+        {
+            List<Task> tasks = _tasksMock.GetTasks();
+            
+            Assert.IsNotNull(tasks);
+            Assert.AreEqual(1, tasks.Count);
+
+            Task result = tasks.First();
+            Dictionary<string, string> resultReference = InputObject["todo-items"][0];
+            
+            Assert.AreEqual(resultReference["id"], result.Id.ToString());
+            Assert.AreEqual(resultReference["project-name"], result.ProjectName);
+            Assert.AreEqual(resultReference["company-name"], result.CompanyName);
+            Assert.AreEqual(resultReference["content"], result.Title);
+            Assert.AreEqual(resultReference["description"], result.Description);
+            Assert.AreEqual(resultReference["priority"], result.Priority);
+            Assert.AreEqual(resultReference["responsible-party-summary"], result.AssignedTo);
+            Assert.AreEqual(resultReference["completed"], result.Completed.ToString());
+            Assert.AreEqual(resultReference["created-on"], result.CreatedOn.Value.ToString("yyyy/MM/dd"));
+            Assert.AreEqual(resultReference["due-date"], result.DueDate.Value.ToString("yyyyMMdd"));
+        }
+        
         [TestInitialize]
         public void Initialize()
         {
@@ -35,9 +81,9 @@ namespace External.tests.libTeamworkTests
                             {"description", "A description of the task"},
                             {"priority", "High"},
                             {"responsible-party-summary", "user@email.com"},
-                            {"completed", "true"},
+                            {"completed", "True"},
                             {"created-on", "2020/01/01"},
-                            {"due-date", "2020/02/01"}
+                            {"due-date", "20200201"}
                         
                         }
                     }
