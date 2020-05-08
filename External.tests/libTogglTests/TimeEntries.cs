233de189bed118386ea037b828fe2431dd112045
David Thole <david@thedarktrumpet.com>
2020-05-07 20:59:00 -0500

    Adding infrastructure for the next test

--- External.tests/libTogglTests/TimeEntries.cs
+++ External.tests/libTogglTests/TimeEntries.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using RestSharp;
 
 namespace External.tests.libTogglTests
 {
@@ -6,6 +9,9 @@ namespace External.tests.libTogglTests
     public class TimeEntries
     {
         private TimeEntriesMock _timeEntriesMock { get; set; }
+        private Mock<RestRequest> MockRestRequest { get; set; }
+        private Mock<RestClient> MockRestClient { get; set; }
+        private Dictionary<string, List<Dictionary<string, string>>> InputObject;
 
         [TestMethod]
         public void Constructor_WithAPIkey_ShouldSetProperties()
@@ -32,6 +38,30 @@ namespace External.tests.libTogglTests
         public void Initialize()
         {
             _timeEntriesMock = new TimeEntriesMock("An API Key");
+
+            MockRestRequest = new Mock<RestRequest>();
+            MockRestRequest = new Mock<RestRequest>();
+            
+            InputObject = new Dictionary<string, List<Dictionary<string, string>>>()
+            {
+                {
+                    "items", new List<Dictionary<string, string>>()
+                    {
+                        new Dictionary<string, string>()
+                        {
+                            {"id", "123432"},
+                            {"isBillable", "True"},
+                            {"client", "A new client"},
+                            {"user", "A user"},
+                            {"project", "A new project"},
+                            {"description", "A description of the task"},
+                            {"billable", "5.23"},
+                            {"start", "2020/01/01 15:00"},
+                            {"end", "2020/01/01 17:00"}
+                        }
+                    }
+                }
+            };
         }
 
         private class TimeEntriesMock : libToggl.api.TimeEntries
