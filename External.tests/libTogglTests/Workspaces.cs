37512fa40aee467e9bde39fcbd97d682be43cda7
David Thole <david@thedarktrumpet.com>
2020-05-08 15:08:32 -0500

    Finished setup of the test infrastructure

--- External.tests/libTogglTests/Workspaces.cs
+++ External.tests/libTogglTests/Workspaces.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using Newtonsoft.Json;
 using RestSharp;
 
 namespace External.tests.libTogglTests
@@ -38,12 +39,22 @@ namespace External.tests.libTogglTests
                     }
                 }
             };
-            
+            IRestResponse response = new RestResponse();
+            response.Content = JsonConvert.SerializeObject(InputObject);
+
+            MockRestClient.Setup(x => x.Execute(MockRestRequest.Object)).Returns(response);
+            _mockWorkspace.SetupMocks(MockRestClient, MockRestRequest);
         }
 
         private class MockWorkSpace : libToggl.api.Workspaces
         {
             public MockWorkSpace(string apiKey) : base(apiKey) { }
+
+            public void SetupMocks(Mock<RestClient> client, Mock<RestRequest> request)
+            {
+                RestClient = client.Object;
+                RestRequest = request.Object;
+            }
         }
     }
 }
