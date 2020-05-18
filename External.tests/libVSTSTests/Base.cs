b449a34e613ffdacca4465f2c6aea82d690aec8d
David Thole <david@thedarktrumpet.com>
2020-05-17 21:31:34 -0500

    Making more properties public so I can test against them easier.  Wrote the first constructor test

--- External.tests/libVSTSTests/Base.cs
+++ External.tests/libVSTSTests/Base.cs
@@ -2,18 +2,28 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace External.tests.libVSTSTests
 {
+    [TestClass]
     public class Base
     {
+        private BaseMock _mock { get; set; }
 
-
+        [TestMethod]
+        public void Constructor_WithArgs_ShouldSetParameters()
+        {
+            Assert.IsNotNull(_mock.ApiKey);
+            Assert.AreEqual("anAPIKey", _mock.ApiKey);
+            Assert.IsNotNull("anOrganization", _mock.Organization);
+        }
+        
         [TestInitialize]
         public void Initialize()
         {
-            
+            _mock = new BaseMock();
         }
         
         private class BaseMock : libVSTS.api.Base
         {
+            public BaseMock() : this("anAPIKey", "anOrganization") { }
             public BaseMock(string apiKey, string organization) : base(apiKey,organization) { }
         }
     }
--- libVSTS/api/Base.cs
+++ libVSTS/api/Base.cs
@@ -6,10 +6,10 @@ namespace libVSTS.api
 {
     public class Base
     {
-        protected string ApiKey { get; set; }
-        protected string BaseURL { get; set; } = "https://dev.azure.com";
-        protected string Organization { get; set; }
-        protected string Project { get; set; }
+        public string ApiKey { get; set; }
+        public string BaseURL { get; set; } = "https://dev.azure.com";
+        public string Organization { get; set; }
+        public string Project { get; set; }
         
         protected CookieContainer CookieContainer { get; set; }
         protected IRestClient RestClient { get; set; }
