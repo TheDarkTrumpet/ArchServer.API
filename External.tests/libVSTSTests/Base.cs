6ecdb719a31ec1be59785601915eb3d5eecc8be4
David Thole <david@thedarktrumpet.com>
2020-05-20 18:35:19 -0500

    Updated the constructor test

--- External.tests/libVSTSTests/Base.cs
+++ External.tests/libVSTSTests/Base.cs
@@ -13,7 +13,12 @@ namespace External.tests.libVSTSTests
         {
             Assert.IsNotNull(_mock.ApiKey);
             Assert.AreEqual("anAPIKey", _mock.ApiKey);
-            Assert.IsNotNull("anOrganization", _mock.Organization);
+            Assert.IsNotNull(_mock.Organization);
+            Assert.AreEqual("anOrganization", _mock.Organization);
+            Assert.IsNotNull(_mock.Project);
+            Assert.AreEqual("aProject", _mock.Project);
+            Assert.IsNotNull(_mock.EndPointUri);
+            Assert.AreEqual("aProject/_apis/wit/wiql", _mock.EndPointUri);
         }
 
         [TestMethod]
